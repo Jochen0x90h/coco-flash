1fc79cd5fac0c33d3c1fd5b17de0af920fc7c8ca
Jochen Wilhelmy <jochen.wilhelmy@gmail.com>
2025-09-10 13:06:11 +0200

    Add tests for stm32h503 and u385

---
+++ test/nucleo-g474re/FlashTest.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <coco/platform/Loop_TIM2.hpp>
+#include <coco/platform/Flash_flash.hpp>
+#include <coco/board/config.hpp>
+
+
+using namespace coco;
+
+// address of last page in flash memory
+constexpr uint32_t FLASH_TEST_ADDRESS = FLASH_ADDRESS + FLASH_SIZE - flash::PAGE_SIZE;
+
+// drivers for FlashTest
+struct Drivers {
+    Loop_TIM2 loop{SYS_CLOCK};
+    Flash_flash flash;
+    Flash_flash::Buffer<256> buffer{flash};
+};
+
+Drivers drivers;
